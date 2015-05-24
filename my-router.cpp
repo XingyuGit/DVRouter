b8628793b03d3e975c98fba5e72a09093abc7064
XingyuGit <xingyuhit@gmail.com>
2015-05-23 23:14:48 -0700

    initialize router (add list of neighbors)

--- my-router.cpp
+++ my-router.cpp
@@ -1,60 +1,81 @@
 #include <boost/asio.hpp>
-//#include <boost/shared_ptr.hpp>
 #include <boost/array.hpp>
 #include <boost/bind.hpp>
+#include <boost/ptr_container/ptr_vector.hpp>
+#include <boost/algorithm/string.hpp>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <stdint.h>
 
 using namespace std;
 using namespace boost::asio::ip;
 
+struct interface {
+    interface(string id, int port, int link_cost)
+    : id(id), port(port), link_cost(link_cost) {}
+    string id;
+    uint16_t port;
+    int link_cost;
+};
+
 class MyRouter
 {
 public:
-    MyRouter(boost::asio::io_service& io_service, uint16_t port)
-    : socket_(io_service, udp::endpoint(udp::v4(), port))
+    MyRouter(boost::asio::io_service& io_service, string id,
+             vector<interface> neighbors)
+        : io_service(io_service), id(id), neighbors(neighbors)
     {
-        start_receive();
+        for (auto& i : neighbors)
+        {
+            udp::socket* sock = new udp::socket(io_service, udp::endpoint(udp::v4(), i.port));
+            sockets.push_back(sock);
+            start_receive(sock);
+        }
     }
     
-    void send(string message, uint16_t port)
+    void broadcast(string message)
     {
-        udp::endpoint sendee_endpoint = udp::endpoint(udp::v4(), port);
-        socket_.async_send_to(boost::asio::buffer(message), sendee_endpoint,
+        for (auto& sock : sockets)
+        {
+            send(message, &sock);
+        }
+    }
+    
+    void send(string message, udp::socket* sock)
+    {
+        sock->async_send_to(boost::asio::buffer(message), sendee_endpoint,
                               boost::bind(&MyRouter::handle_send, this, message,
                                           boost::asio::placeholders::error,
                                           boost::asio::placeholders::bytes_transferred));
     }
     
 private:
-    void start_receive()
+    void start_receive(udp::socket* sock)
     {
-        socket_.async_receive_from(boost::asio::buffer(recv_buffer_), remote_endpoint_,
-                                   boost::bind(&MyRouter::handle_receive, this,
+        sock->async_receive_from(boost::asio::buffer(recv_buffer), remote_endpoint,
+                                   boost::bind(&MyRouter::handle_receive, this, sock,
                                                boost::asio::placeholders::error,
                                                boost::asio::placeholders::bytes_transferred));
     }
     
-    void handle_receive(const boost::system::error_code& error, size_t bytes_recvd)
+    void handle_receive(udp::socket* sock, const boost::system::error_code& error,
+                        size_t bytes_recvd)
     {
         if (!error || error == boost::asio::error::message_size)
         {
-            //            boost::shared_ptr<string> message(new string(make_daytime_string()));
-            //            socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-            //                                  boost::bind(&MyRouter::handle_send, this, message));
-            
-            string recv_str(recv_buffer_.begin(), recv_buffer_.begin() + bytes_recvd);
+            string recv_str(recv_buffer.begin(), recv_buffer.begin() + bytes_recvd);
             cout << "async_receive_from message='" << recv_str << "'" << endl;
             cout << "async_receive_from return " << error << ": " << bytes_recvd << " received" << endl;
             
             string message = "hello";
-            socket_.async_send_to(boost::asio::buffer(message), remote_endpoint_,
+            sock->async_send_to(boost::asio::buffer(message), sock->remote_endpoint(),
                                   boost::bind(&MyRouter::handle_send, this, message,
                                               boost::asio::placeholders::error,
                                               boost::asio::placeholders::bytes_transferred));
             
-            start_receive();
+            // continue listening
+            start_receive(sock);
         }
     }
     
@@ -65,21 +86,50 @@ private:
         cout << "async_send_to return " << error << ": " << bytes_transferred << " transmitted" << endl;
     }
     
-    udp::socket socket_;
-    udp::endpoint remote_endpoint_;
+    boost::asio::io_service& io_service;
+    string id;
+    vector<interface> neighbors;
+    boost::ptr_vector<udp::socket> sockets;
     enum { max_length = 1024};
-    boost::array<char, max_length> recv_buffer_;
+    boost::array<char,max_length> recv_buffer;
+    
+    // temp var
+    udp::endpoint remote_endpoint;
+    udp::endpoint sendee_endpoint;
 };
 
-int main(void)
+int main(int argc, char** argv)
 {
-    boost::asio::io_service io_service;
-    int local_port = 10000;
+    if (argc != 2)
+    {
+        cout << "Wrong arguments. Correct: ./my-router <id>" << endl;
+    }
     
-    MyRouter rt(io_service, local_port);
+    string id = string(argv[1]);
+    vector<interface> interfaces;
+    
+    ifstream initfile("init.txt");
+    string line;
+    while (getline(initfile, line))
+    {
+        vector<string> tokens;
+        boost::split(tokens, line, boost::is_any_of(","));
+        string src_router = tokens[0];
+        string dest_router = tokens[1];
+        string src_port = tokens[2];
+        string link_cost = tokens[3];
+        
+        if (id.compare(src_router) == 0) {
+            interfaces.push_back(interface(dest_router, stoi(src_port), stoi(link_cost)));
+        }
+    }
+    
+    boost::asio::io_service io_service;
+    MyRouter rt(io_service, id, interfaces);
     io_service.run();
     
     return 0;
 }
+
 
 
