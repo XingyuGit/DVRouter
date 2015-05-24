42a00183417144a162f178d550da7ecd096af73b
XingyuGit <xingyuhit@gmail.com>
2015-05-23 17:38:09 -0700

    send function

--- my-router.cpp
+++ my-router.cpp
@@ -3,14 +3,25 @@
 #include <boost/array.hpp>
 #include <iostream>
 #include <string>
+#include <stdint.h>
 
-class udp_server
+using namespace std;
+
+class MyRouter
 {
 public:
-    udp_server(boost::asio::io_service& io_service, short port)
+    udp_server(boost::asio::io_service& io_service, uint16_t port)
     : socket_(io_service, udp::endpoint(udp::v4(), port))
     {
         start_receive();
+    }
+    
+    void send(string message, uint16_t port)
+    {
+        send_endpoint = udp::endpoint(udp::v4(), port)
+        socket_.async_send_to(boost::asio::buffer(message), send_endpoint,
+                              boost::bind(&handle_send, boost::asio::placeholders::error,
+                                          boost::asio::placeholders::bytes_transferred));
     }
     
 private:
@@ -26,16 +37,25 @@ private:
     {
         if (!error || error == boost::asio::error::message_size)
         {
-            boost::shared_ptr<std::string> message(new std::string(make_daytime_string()));
+            //            boost::shared_ptr<string> message(new string(make_daytime_string()));
+            //            socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
+            //                                  boost::bind(&udp_server::handle_send, this, message));
             
-            socket_.async_send_to(boost::asio::buffer(*message), remote_endpoint_,
-                                  boost::bind(&udp_server::handle_send, this, message));
+            string message = "message";
+            socket_.async_send_to(boost::asio::buffer(message), remote_endpoint_,
+                                  boost::bind(&udp_server::handle_send, this, message,
+                                              boost::asio::placeholders::error,
+                                              boost::asio::placeholders::bytes_transferred));
+            
             start_receive();
         }
     }
     
-    void handle_send(boost::shared_ptr<std::string> /*message*/)
+    void handle_send(string message, const boost::system::error_code& error,
+                     std::size_t bytes_transferred)
     {
+        cout << "async_send_to message='" << message << "'" << endl;
+        cout << "async_send_to return " << error << ": " << bytes_transferred << " transmitted" << endl;
     }
     
     udp::socket socket_;
@@ -47,9 +67,9 @@ private:
 int main(void)
 {
     boost::asio::io_service io_service;
-    short port = 10000;
+    int local_port = 10000;
     
-    udp_server server(io_service, port);
+    MyRouter rt(io_service, local_port);
     io_service.run();
     
     return 0;
